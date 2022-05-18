8a5521bc4c393a423f4fdf9bf50a3d547991f65c
Joseph T <jdtoronto999@outlook.com>
2022-05-18 21:43:18 +0100

    DOM practice

---
+++ bomdom/js/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+button.addEventListener('click', function() {
+    if(input.value != '') {
+        const inputValue = input.value;
+        input.value = '';
+
+        let listItem = document.createElement('li');
+        let deleteButton = document.createElement('button');
+
+        deleteButton.setAttribute('aria-label', `Remove ${inputValue}`);
+
+        listItem.innerHTML = inputValue;
+        deleteButton.textContent = '❌';
+
+        listItem.appendChild(deleteButton);
+        list.appendChild(listItem);
+
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(listItem);
+        });
+
+        input.focus();
+    }
+});
