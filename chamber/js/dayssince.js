73e5604053e9a1d6615e93afda820575ee8b196e
Joseph T <jdtoronto999@outlook.com>
2022-06-05 00:52:06 +0100

    days since code fixed

--- chamber/js/dayssince.js
+++ chamber/js/dayssince.js
@@ -2,6 +2,11 @@ const daysSince = document.getElementById("days-since");
 
 let previousDay = Number(window.localStorage.getItem("last-time"));
 
-daysSince.innerHTML = Math.floor((Date.now() - previousDay) / 86400000);
+if(previousDay !== 0) {
+    daysSince.innerHTML = Math.floor((Date.now() - previousDay) / 86400000);
+} else {
+    daysSince.innerHTML = 0;
+}
+
 
 localStorage.setItem("last-time", Date.now());
